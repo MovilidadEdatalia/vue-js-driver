b0f4a400600ed12ce6356139a908174a87cbc912
movilidad.edatalia <movilidad@edatalia.com>
2024-09-24 17:07:38 +0200

    Adds onFinish callback, and stopDriver method to vue driver js wrapper

--- index.js
+++ index.js
@@ -4,45 +4,73 @@ import 'driver.js/dist/driver.min.css';
 const install = function (Vue, options = {}) {
   let vueJsDriver = {};
 
-  function initDriver(options) {
+  // Función para inicializar Driver.js
+  function initDriver(options, onFinish) {
     const driverOptions = {
-      className: options && options.className || 'vue-js-driver',
+      className: options.className || 'vue-js-driver',
       allowClose: false,
       prevBtnText: '上一步',
       nextBtnText: '下一步',
       closeBtnText: '关闭',
       doneBtnText: '完成',
-      ...options
+      ...options,
+      onReset: () => {
+        if (typeof options.onReset === 'function') {
+          options.onReset();
+        }
+        if (onFinish) {
+          onFinish();  // Llamar al callback cuando el tour termina
+        }
+      },
+      onClose: () => {
+        if (onFinish) {
+          onFinish();  // Llamar al callback cuando el tour se cierra
+        }
+      }
     };
-    vueJsDriver = new Driver(driverOptions);
-  };
+    vueJsDriver.driverInstance = new Driver(driverOptions);
+  }
 
   /**
-   * 打开新手引导
-   * @param { Array } steps 所有步骤的参数
-   * @param { Number } index 开始高亮的索引
-   * @returns { void }
+   * Función para abrir el tour
+   * @param { Array } steps - Los pasos del tour
+   * @param { Number } index - El índice de paso inicial
+   * @param { Function } onFinish - Callback para ejecutar cuando el tour termine
    * @public
    */
-  function showDriver (steps = [], index = 0) {
+  function showDriver(steps = [], index = 0, onFinish = null) {
     if (!steps.length) {
       return;
     }
 
-    vueJsDriver.defineSteps(steps);
-    vueJsDriver.start(index);
+    // Inicializar Driver.js y pasar el callback
+    initDriver(options, onFinish);
+
+    // Definir los pasos y comenzar el tour
+    vueJsDriver.driverInstance.defineSteps(steps);
+    vueJsDriver.driverInstance.start(index);
   }
 
-  initDriver(options);
+  // Asignar la función showDriver al objeto vueJsDriver
   vueJsDriver.showDriver = showDriver;
+
+  vueJsDriver.stopDriver = () => {
+    console.log(vueJsDriver.driverInstance);
+    if (vueJsDriver.driverInstance) {
+      vueJsDriver.driverInstance.reset(true);  // O reset()
+    }
+  };
+
+  // Añadir vueJsDriver al prototipo de Vue
   Vue.prototype.$vueJsDriver = vueJsDriver;
 };
 
 /* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)  ;
+  install(window.Vue);
 }
 
 export default {
   install
-}
+};
+
